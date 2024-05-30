ed73ccdb60f782638d6e8afbd3ff2a856fb8823f
RODHERMA81 <arjo1981@gmail.com>
2024-05-30 14:32:24 +0200

    ejercicio ivan corregido

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,15 +8,16 @@ public class App
 {
 	private static int ARRAY_SIZE;
 	private static double CUARTO_ARGUMENTO;
-	private static String nombre;
+	// private static String nombre;
 
 
 	public static void main(String[] args)
 	{
 		// Las variables que se declaran dentro de metodos que son locales y no se inicializan a un valor
 		// por defecto de forma implicita
-		
-		String otroNombre = null;
+		// System.out.println(ARRAY_SIZE);
+		// String otroNombre = "Rosa";
+		// System.out.println(el valor de la variable otroNombre es " + otroNombre);
 		
 		
 		/*Manejo de los argumentos que recibe el metodo main, es decir, la App cuando se ejecuta.
@@ -44,6 +45,16 @@ public class App
 			System.out.println("Mostrar el valor de la variable args[1]:");
 			System.out.println(ARRAY_SIZE);
 			System.out.println("---------------");
+			// Antes de convertir el cuarto argumento a double, hay que comprobar que el argumento
+			// contiene un double encerrado en un string.
+			try {
+				CUARTO_ARGUMENTO = Double.parseDouble(args[3]);
+				System.out.println(CUARTO_ARGUMENTO);
+			} catch ( NumberFormatException e) {
+				// TODO: handle exception
+				System.out.println("El argumento no se puede convertir a double");
+				System.out.println (e.getMessage());
+			}
 			CUARTO_ARGUMENTO = Double.parseDouble(args[3]);
 			System.out.println(CUARTO_ARGUMENTO);
 			
